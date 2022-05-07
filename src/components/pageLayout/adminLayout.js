fc71262ad5bac2a218f8d92b41cdc42d83fd570c
candrawahyuf <83354350+CandraWahyuF@users.noreply.github.com>
2022-05-07 08:10:20 +0700

    update admin design

--- src/components/pageLayout/adminLayout.js
+++ src/components/pageLayout/adminLayout.js
@@ -1,33 +1,130 @@
 import React from "react";
-import clsx from "clsx";
-import { useTheme } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import Badge from "@material-ui/core/Badge";
-import NotificationsIcon from "@material-ui/icons/Notifications";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import MoreIcon from "@material-ui/icons/MoreVert";
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
-import HomeIcon from "@material-ui/icons/Home";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
-import RestaurantMenuIcon from "@material-ui/icons/RestaurantMenu";
-import { NavbarStyle } from "./NavbarStyle";
+import { styled, useTheme } from "@mui/material/styles";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import {
+  AccountCircle,
+  ChevronLeft,
+  ChevronRight,
+  Group,
+  Home,
+  Menu,
+  Notifications,
+  RestaurantMenu,
+  ShoppingCart,
+} from "@mui/icons-material";
+import {
+  Badge,
+  Box,
+  CssBaseline,
+  Divider,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MoreIcon from "@mui/icons-material/MoreVert";
 
-export const AdminLayout = () => {
-  const classes = NavbarStyle();
+const drawerWidth = 240;
+
+// THEME STYLE
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  backgroundColor: "#311C82",
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
+// END THEME STYLE
+
+// LIST ITEM MENU KIRI
+const ListMenu = [
+  {
+    text: "Beranda",
+    icon: <Home />,
+    path: "/",
+  },
+  {
+    text: "Produk",
+    icon: <RestaurantMenu />,
+    path: "/Produk",
+  },
+  {
+    text: "Transaksi",
+    icon: <ShoppingCart />,
+    path: "/Transaksi",
+  },
+  {
+    text: "Data Pegawai",
+    icon: <Group />,
+    path: "/DataPegawai",
+  },
+];
+
+// MAIN
+export default function Navbar() {
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
@@ -39,7 +136,7 @@ export const AdminLayout = () => {
     setOpen(false);
   };
 
-  // notif kanan
+  // ITEM ICONS SEBELAH KANAN
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
@@ -67,10 +164,16 @@ export const AdminLayout = () => {
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
       id={menuId}
       keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
@@ -83,23 +186,34 @@ export const AdminLayout = () => {
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
       id={mobileMenuId}
       keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
       <MenuItem>
-        <IconButton aria-label="show 11 new notifications" color="inherit">
-          <Badge badgeContent={11} color="secondary">
-            <NotificationsIcon />
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={5} color="error">
+            <Notifications />
           </Badge>
         </IconButton>
-        <p>Notifications</p>
+        <p>Notifikasi</p>
       </MenuItem>
       <MenuItem onClick={handleProfileMenuOpen}>
         <IconButton
+          size="large"
           aria-label="account of current user"
           aria-controls="primary-search-account-menu"
           aria-haspopup="true"
@@ -112,62 +226,43 @@ export const AdminLayout = () => {
     </Menu>
   );
 
-  // LIST MENU LEFTBAR
-  const NavbarMenu = [
-    {
-      text: "Beranda",
-      icons: <HomeIcon />,
-      path: "/",
-    },
-    {
-      text: "Produk",
-
-      icons: <RestaurantMenuIcon />,
-      path: "/",
-    },
-    {
-      text: "Transaksi",
-      icons: <ShoppingCartIcon />,
-      path: "/",
-    },
-  ];
-
   return (
-    <div className={classes.root}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
 
-      {/* TOPBAR */}
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
+      {/* TOPBAR BOX */}
+      <AppBar position="fixed" open={open}>
         <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
             onClick={handleDrawerOpen}
             edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open,
-            })}
+            sx={{
+              marginRight: 5,
+              ...(open && { display: "none" }),
+            }}
           >
-            <MenuIcon />
+            <Menu />
           </IconButton>
-          <Typography variant="h6" noWrap>
+          <Typography variant="h6" noWrap component="div">
             WAROENG MAKAN
           </Typography>
 
-          {/* ICONS SEBELAH KANAN */}
-          <div className={classes.grow} />
-          <div className={classes.sectionDesktop}>
-            <IconButton aria-label="show 17 new notifications" color="inherit">
-              <Badge badgeContent={17} color="secondary">
-                <NotificationsIcon />
+          {/* ITEM ICON KANAN */}
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={17} color="error">
+                <Notifications />
               </Badge>
             </IconButton>
             <IconButton
+              size="large"
               edge="end"
               aria-label="account of current user"
               aria-controls={menuId}
@@ -177,9 +272,10 @@ export const AdminLayout = () => {
             >
               <AccountCircle />
             </IconButton>
-          </div>
-          <div className={classes.sectionMobile}>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
             <IconButton
+              size="large"
               aria-label="show more"
               aria-controls={mobileMenuId}
               aria-haspopup="true"
@@ -188,59 +284,57 @@ export const AdminLayout = () => {
             >
               <MoreIcon />
             </IconButton>
-          </div>
+          </Box>
         </Toolbar>
       </AppBar>
       {renderMobileMenu}
       {renderMenu}
 
-      {/* SIDEBAR */}
-      <Drawer
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
-        classes={{
-          paper: clsx({
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          }),
-        }}
-      >
-        <div className={classes.toolbar}>
+      {/* LEFTBAR */}
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
-              <ChevronRightIcon className={classes.icons} />
-            ) : (
-              <ChevronLeftIcon className={classes.icons} />
-            )}
+            {theme.direction === "rtl" ? <ChevronRight /> : <ChevronLeft />}
           </IconButton>
-        </div>
-        <Divider className={classes.divider} />
+        </DrawerHeader>
 
-        {/* MENU LIST ITEM */}
+        {/* LIST ITEM MENU */}
+        <Divider />
         <List>
-          {NavbarMenu.map((item) => (
-            <ListItem button key={item.text}>
-              <ListItemIcon className={classes.icons}>
-                {item.icons}
+          {ListMenu.map((item) => (
+            <ListItemButton
+              key={item.text}
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                {item.icon}
               </ListItemIcon>
-              <ListItemText>{item.text}</ListItemText>
-            </ListItem>
+              <ListItemText
+                primary={item.text}
+                sx={{ opacity: open ? 1 : 0 }}
+              />
+            </ListItemButton>
           ))}
         </List>
-        <Divider className={classes.divider} />
+        <Divider />
       </Drawer>
 
       {/* KONTEN DISINI */}
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <Typography paragraph>ISI PERTAMA</Typography>
-        <Typography paragraph>ISI KEDUA</Typography>
-      </main>
-    </div>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        <Typography paragraph>PARAGRAPH PERTAMA</Typography>
+        <Typography paragraph>PARAGRAPH KEDUA</Typography>
+      </Box>
+    </Box>
   );
-};
-
-export default AdminLayout;
+}
