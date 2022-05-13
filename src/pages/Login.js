f10786257ea00f35e3a4f9db630a1e86edd458f3
mbagas <karab4946@gmail.com>
2022-05-13 14:47:35 +0700

    feat: changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -96,7 +96,7 @@ export default function Login() {
                 {/* <LockOutlinedIcon /> */}
               </Avatar>
               <Typography component="h1" variant="h5">
-                Selamat Datang di Waroeng Makan
+                Selamat Datang
               </Typography>
               <Box
                 component="form"
